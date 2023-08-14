013bf1901086e832dfe633f1a0282c14937864b6
mattdattilio <120666057+mattdattilio@users.noreply.github.com>
2023-08-14 12:18:25 -0400

    Final Commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// create function for computer to pick rock paper or scissors
+let computerScore = 0;
+let playerScore = 0;
 
 const rps = ["rock", "paper", "scissors"];
 
@@ -7,7 +8,35 @@ function getComputerChoice() {
   return choice;
 }
 
-//Write a function to check the winner. Return the winner
+function getPlayerChoice() {
+  let choice = prompt("Please enter Rock, Paper, or Scissors").toLowerCase();
+  let validate = 0;
+  while (validate == 0) {
+    if (choice == "rock" || choice == "paper" || choice == "scissors") {
+      validate = 1;
+      return choice;
+    } else {
+      choice = prompt("Please enter Rock, Paper, or Scissors").toLocaleLowerCase();
+    }
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  const result = checkWinner(playerSelection, computerSelection);
+  if (result == "Draw") {
+    return "It's a Tie!";
+  } else if (result == "Player") {
+    playerScore++;
+    keepScore();
+    checkGameOver();
+    return `You Win! ${playerSelection} beats ${computerSelection}!`;
+  } else {
+    computerScore++;
+    keepScore();
+    checkGameOver();
+    return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+  }
+}
 
 function checkWinner(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
@@ -23,56 +52,48 @@ function checkWinner(playerSelection, computerSelection) {
   }
 }
 
-//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters
-
-function playRound(playerSelection, computerSelection) {
-  const result = checkWinner(playerSelection, computerSelection);
-  if (result == "Draw") {
-    return "It's a Tie!";
-  } else if (result == "Player") {
-    return `You Win! ${playerSelection} beats ${computerSelection}!`;
-  } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+function resetScore() {
+  if (playerScore == 5 || computerScore == 5) {
+    playerScore = 0;
+    computerScore = 0;
+    keepScore();
   }
 }
 
-//Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-function getPlayerChoice() {
-  let choice = prompt("Please enter Rock, Paper, or Scissors").toLowerCase();
-  let validate = 0;
-  while (validate == 0) {
-    if (choice == "rock" || choice == "paper" || choice == "scissors") {
-      validate = 1;
-      return choice;
-    } else {
-      choice = prompt(
-        "Please enter Rock, Paper, or Scissors"
-      ).toLocaleLowerCase();
-    }
+function checkGameOver() {
+  if (computerScore >= 5) {
+    alert("Game Over. You Lose.");
+  } else if (playerScore >= 5) {
+    alert("Game Over. You Win.");
   }
 }
+const results = document.querySelector("#results");
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
+const rockBtn = document.querySelector(`#rock`);
+rockBtn.addEventListener(`click`, () => {
+  playerSelection = "rock";
+  computerSelection = getComputerChoice();
+  results.textContent = playRound(playerSelection, computerSelection);
+  resetScore();
+});
 
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-    console.log("---------");
-    if (checkWinner(playerSelection, computerSelection) == "Player") {
-      playerScore++;
-    } else if (checkWinner(playerSelection, computerSelection) == "Computer") {
-      computerScore++;
-    }
-  }
-  console.log("Game Over");
-  if (playerScore > computerScore) {
-    return `You Win!`;
-  } else if (playerScore < computerScore) {
-    return `You Lose!`;
-  } else {
-    return `It's a Tie!`;
-  }
+const paperBtn = document.querySelector(`#paper`);
+paperBtn.addEventListener(`click`, () => {
+  playerSelection = "paper";
+  computerSelection = getComputerChoice();
+  results.textContent = playRound(playerSelection, computerSelection);
+  resetScore();
+});
+
+const scissorsBtn = document.querySelector(`#scissors`);
+scissorsBtn.addEventListener(`click`, () => {
+  playerSelection = "scissors";
+  computerSelection = getComputerChoice();
+  results.textContent = playRound(playerSelection, computerSelection);
+  resetScore();
+});
+
+function keepScore() {
+  const score = document.querySelector("#score");
+  score.textContent = `${playerScore} : ${computerScore}`;
 }
